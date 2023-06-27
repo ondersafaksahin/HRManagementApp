582e36e9b69ad62e15c9c3a070f64af492d1533a
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-06-27 12:16:53 +0300

    Repository interfaces and classes created

---
+++ HRManagementApp.Infrastructure/Repositories/ExpenseRepository.cs
@@ -0,0 +1,23 @@
+﻿using HRManagementApp.Domain.Entities.Concrete;
+using HRManagementApp.Domain.IRepositories;
+using HRManagementApp.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementApp.Infrastructure.Repositories
+{
+    public class ExpenseRepository : BaseRepository<Expense>, IExpenseRepository
+    {
+        HRDBContext _database;
+        DbSet<Expense> _expenses;
+        public ExpenseRepository(HRDBContext database) : base(database)
+        {
+            _database = database;
+            _expenses = _database.Expenses;
+        }
+    }
+}
---
+++ HRManagementApp.Infrastructure/Repositories/PackageRepository.cs
@@ -0,0 +1,23 @@
+﻿using HRManagementApp.Domain.Entities.Concrete;
+using HRManagementApp.Domain.IRepositories;
+using HRManagementApp.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManagementApp.Infrastructure.Repositories
+{
+    public class PackageRepository : BaseRepository<Package>, IPackageRepository
+    {
+        HRDBContext _database;
+        DbSet<Package> _packages;
+        public PackageRepository(HRDBContext database) : base(database)
+        {
+            _database = database;
+            _packages = _database.Packages;
+        }
+    }
+}
