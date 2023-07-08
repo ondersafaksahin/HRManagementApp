77e73f131710ba2c271446480d318764bc57373c
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-07-08 16:39:45 +0300

    Automapper added to program.cs and Maps created in Application/Automapper/Mapping class

--- HRManagementApp.Application/AutoMapper/Mapping.cs
+++ HRManagementApp.Application/AutoMapper/Mapping.cs
@@ -1,4 +1,15 @@
 ﻿using AutoMapper;
+using HRManagementApp.Application.DTOs.AdminDTOs;
+using HRManagementApp.Application.DTOs.AdvancePaymentDTOs;
+using HRManagementApp.Application.DTOs.AppRoleDTOs;
+using HRManagementApp.Application.DTOs.CompanyDTOs;
+using HRManagementApp.Application.DTOs.EmployeeDTOs;
+using HRManagementApp.Application.DTOs.ExpenseDTOs;
+using HRManagementApp.Application.DTOs.LeaveDTOs;
+using HRManagementApp.Application.DTOs.ManagerDTOs;
+using HRManagementApp.Application.DTOs.PackageDTOs;
+using HRManagementApp.Application.DTOs.ShiftDTOs;
+using HRManagementApp.Domain.Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +22,55 @@ namespace HRManagementApp.Application.AutoMapper
     {
         public Mapping()
         {
-            
+            CreateMap<Admin, AdminDTO>().ReverseMap();
+            CreateMap<Admin, AdminCreateDTO>().ReverseMap();
+            CreateMap<Admin, AdminUpdateDTO>().ReverseMap();
+            CreateMap<Admin, AdminListDTO>().ReverseMap();
+
+            CreateMap<AdvancePayment, AdvancePaymentDTO>().ReverseMap();
+            CreateMap<AdvancePayment, AdvancePaymentCreateDTO>().ReverseMap();
+            CreateMap<AdvancePayment, AdvancePaymentUpdateDTO>().ReverseMap();
+            CreateMap<AdvancePayment, AdvancePaymentListDTO>().ReverseMap();
+
+            CreateMap<AppRole, AppRoleDTO>().ReverseMap();
+            CreateMap<AppRole, AppRoleCreateDTO>().ReverseMap();
+            CreateMap<AppRole, AppRoleUpdateDTO>().ReverseMap();
+            CreateMap<AppRole, AppRoleListDTO>().ReverseMap();
+
+            CreateMap<Company, CompanyDTO>().ReverseMap();
+            CreateMap<Company, CompanyCreateDTO>().ReverseMap();
+            CreateMap<Company, CompanyUpdateDTO>().ReverseMap();
+            CreateMap<Company, CompanyListDTO>().ReverseMap();
+
+            CreateMap<Employee, EmployeeDTO>().ReverseMap();
+            CreateMap<Employee, EmployeeCreateDTO>().ReverseMap();
+            CreateMap<Employee, EmployeeUpdateDTO>().ReverseMap();
+            CreateMap<Employee, EmployeeListDTO>().ReverseMap();
+
+            CreateMap<Expense, ExpenseDTO>().ReverseMap();
+            CreateMap<Expense, ExpenseCreateDTO>().ReverseMap();
+            CreateMap<Expense, ExpenseUpdateDTO>().ReverseMap();
+            CreateMap<Expense, ExpenseListDTO>().ReverseMap();
+
+            CreateMap<Leave, LeaveDTO>().ReverseMap();
+            CreateMap<Leave, LeaveCreateDTO>().ReverseMap();
+            CreateMap<Leave, LeaveUpdateDTO>().ReverseMap();
+            CreateMap<Leave, LeaveListDTO>().ReverseMap();
+
+            CreateMap<Manager, ManagerDTO>().ReverseMap();
+            CreateMap<Manager, ManagerCreateDTO>().ReverseMap();
+            CreateMap<Manager, ManagerUpdateDTO>().ReverseMap();
+            CreateMap<Manager, ManagerListDTO>().ReverseMap();
+
+            CreateMap<Package, PackageDTO>().ReverseMap();
+            CreateMap<Package, PackageCreateDTO>().ReverseMap();
+            CreateMap<Package, PackageUpdateDTO>().ReverseMap();
+            CreateMap<Package, PackageListDTO>().ReverseMap();
+
+            CreateMap<Shift, ShiftDTO>().ReverseMap();
+            CreateMap<Shift, ShiftCreateDTO>().ReverseMap();
+            CreateMap<Shift, ShiftUpdateDTO>().ReverseMap();
+            CreateMap<Shift, ShiftListDTO>().ReverseMap();
         }
     }
 }
--- HRManagementApp.Presentation/Program.cs
+++ HRManagementApp.Presentation/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<HRDBContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
