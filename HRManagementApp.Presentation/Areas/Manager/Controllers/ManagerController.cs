bc234ed8bf74255a05252b2bb0f138ea835f93ca
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-07-22 13:21:58 +0300

    Areas, inner folders, base controllers and authorisations created

---
+++ HRManagementApp.Presentation/Areas/Manager/Controllers/ManagerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace HRManagementApp.Presentation.Areas.Manager.Controllers
+{
+    public class ManagerController : Controller
+    {
+        [Area("Manager")]
+        [Authorize(Roles = "Manager")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
