ce18561e2a689b1dd8a6552dcd2e4c2e71e4e5f8
Önder Şafak Şahin <ondersafak.sahin@bilgeadamboost.com>
2023-08-08 15:45:08 +0300

    Basic structure of the main page created, the links and property connections will be added later

---
+++ HRManagementApp.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRManagementApp.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
